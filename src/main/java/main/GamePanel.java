304fc211c0ca3546c42141dde2f89e53d9d14203
Genbuos <swiftypython18@gmail.com>
2023-10-20 23:20:48 -0400

    Input functionality added

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,18 +1,47 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xdelta = 100, ydelta = 100;
+
 
     public GamePanel(){
-
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
+    public void changeXDelta(int value){
+        this.xdelta+= value;
+        //updates changes on the panel
+        repaint();
+    }
+
+    public void changeYDelta(int value){
+        this.ydelta+= value;
+        //updates the changes on the panel
+        repaint();
+    }
+
+    public void setRectPostition(int x, int y){
+        this.xdelta = x;
+        this.ydelta =y;
+        repaint();
+
+    }
     public void paintComponent(Graphics graphics){
         //calling the paintComponent method defined in the
         // Jpanel Class
         super.paintComponent(graphics);
-        graphics.fillRect(100, 100, 200, 50);
+        graphics.fillRect( xdelta, ydelta, 200, 50);
     }
 }
--- src/main/java/main/GameWindow.java
+++ src/main/java/main/GameWindow.java
@@ -9,6 +9,8 @@ public class GameWindow{
         jFrame.setSize(400,400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        //spawns window in the center
+        jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
 }
