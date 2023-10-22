b3bf5f825ce5c8837c79141e0c4d4435f136525b
Genbuos <swiftypython18@gmail.com>
2023-10-22 00:55:11 -0400

    images added. Animation is next

--- src/main/java/main/GameWindow.java
+++ src/main/java/main/GameWindow.java
@@ -6,11 +6,15 @@ public class GameWindow{
     private JFrame jFrame;
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
-        jFrame.setSize(400,400);
+
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         //spawns window in the center
         jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        //fits the size of the window to the preferred size for its
+        // component.
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 }
