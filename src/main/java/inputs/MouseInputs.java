1048e3a4a84933465155e8138517bc208688ef4d
genbuos <swiftypython18@gmail.com>
2024-12-25 07:21:54 -0500

    Gamestates were created(Menu, Playing)

--- src/main/java/inputs/MouseInputs.java
+++ src/main/java/inputs/MouseInputs.java
@@ -1,5 +1,6 @@
 package inputs;
 
+import gamestates.GameState;
 import main.GamePanel;
 
 import java.awt.event.MouseEvent;
@@ -14,8 +15,16 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getButton() == MouseEvent.BUTTON1)
-            gamePanel.getGame().getPlayer().setAttacking(true);
+    switch (GameState.state){
+        case MENU:
+            gamePanel.getGame().getMenu().mouseClicked(e);
+            break;
+        case PLAYING:
+            gamePanel.getGame().getPlaying().mouseClicked(e);
+            break;
+        default:
+            break;
+    }
     }
 
     @Override
--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,7 +1,8 @@
 package main;
 
-import entities.Player;
-import levels.LevelManager;
+import gamestates.GameState;
+import gamestates.Menu;
+import gamestates.Playing;
 
 import java.awt.*;
 
@@ -14,8 +15,11 @@ public class Game implements Runnable{
     private final int FPS_SET = 120;
 
     private final int UPS_SET = 200;
-    private Player player;
-    private LevelManager levelManager;
+
+
+    private Playing playing;
+    private Menu menu;
+
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 1.5f;
     public final static int TILES_IN_WIDTH = 26;
@@ -36,9 +40,8 @@ public class Game implements Runnable{
     }
 
     private void initClasses() {
-        levelManager = new LevelManager(this);
-        player = new Player(200, 200, (int) (64* SCALE), (int) (64 * SCALE));
-        player.loadLvlData(levelManager.getCurrentLevel().getLevlData());
+    menu = new Menu(this);
+    playing = new Playing(this);
     }
 
     private void startGameLoop(){
@@ -47,14 +50,34 @@ public class Game implements Runnable{
     }
 
     public void update(){
-       player.update();
-       levelManager.update();
+
+
+       switch (GameState.state){
+           case MENU:
+               menu.update();
+               break;
+           case PLAYING:
+                playing.update();
+               break;
+           default:
+               break;
+       }
     }
 
     public void render(Graphics g){
 
-        levelManager.draw(g);
-        player.render(g);
+        switch (GameState.state){
+            case MENU:
+                menu.draw(g);
+                break;
+            case PLAYING:
+               playing.draw(g);
+                break;
+            default:
+                break;
+        }
+
+
     }
     @Override
     public void run() {
@@ -100,10 +123,16 @@ public class Game implements Runnable{
     }
 
     public void windowFocusLost(){
-        player.resetDirBooleans();
+    if(GameState.state == GameState.PLAYING){
+        playing.getPlayer().resetDirBooleans();
+    }
     }
 
-    public Player getPlayer(){
-        return player;
+    public Menu getMenu(){
+        return menu;
     }
+    public Playing getPlaying(){
+        return playing;
+    }
+
 }
